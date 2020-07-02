0bb0186e14f8d665b9fd1bd3ff56cd7dbd79de36
Ihor <ihor.raynovskyy@gmail.com>
2020-07-02 14:37:05 +0300

    Changes to homepage: welcome!

--- music/models.py
+++ music/models.py
@@ -10,6 +10,7 @@ class Album(models.Model):
     genre = models.CharField(max_length=100)
     album_logo = models.FileField()
 
+
     def get_absolute_url(self):
         return reverse('music:detail', kwargs={'pk': self.pk})
 
